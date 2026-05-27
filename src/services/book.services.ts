278ab3ef23ea0360991d06a7e821549768c426c2
analiciasouza <analiciasouza.11@gmail.com>
2026-05-27 08:46:41 -0300

    fix: ajustando book service.

--- src/services/book.services.ts
+++ src/services/book.services.ts
@@ -2,13 +2,21 @@ import { api } from './api';
 import { Book } from '../@types/type';
 
 export const bookService = {
+
   async list(): Promise<Book[]> {
     const response = await api.get('books/');
-    return response.data;
+    return response.data.map((book: any) => ({
+      ...book,
+      coverImage: book.cover_image,
+    }));
   },
 
   async detail(id: string): Promise<Book> {
     const response = await api.get(`books/${id}/`);
-    return response.data;
+    return {
+      ...response.data,
+      coverImage: response.data.cover_image,
+    };
   },
+
 };
